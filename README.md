68fde982f81e57405bf76d43f4196bf8840d950e
Jhonatan Keretz <92455620+JhonatanKeretz@users.noreply.github.com>
2022-06-29 00:17:54 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,11 @@
+Criação de uma calculadora em TypeScript - Material de estudo
+
+Nescessário instalar estes plugins para rodar o projeto
+
+npm i --save-dev @types/node
+
+npm install reflect-metadata @types/reflect-metadata
+
+para rodar projeto
+
+npm run dev
---
+++ assets/typescript/calculadora/Tela.ts
@@ -0,0 +1,36 @@
+export default class Tela {
+
+    constructor(
+        private elemento: HTMLDivElement | null = document.querySelector("#values")
+    ){
+
+        this.conteudo = "0";
+
+    }
+
+    set conteudo(valor: string) {
+
+        if(valor.toString().length > 12) {
+            valor = "ERRO";
+        }
+
+        if(this.elemento){
+
+            this.elemento.innerHTML = valor.toString().replace(".", ",");
+        }
+
+    }
+
+    get conteudo(): string {
+
+        return this.elemento ? this.elemento.innerHTML : "0";
+
+       // if(this.elemento){
+         //   return this.elemento.innerHTML;
+       // } else {
+       //     return "0";
+       // }
+
+    }
+
+}
